e7a1ec8594c9d1a1bce927413cf674a65fdf1bf4
gautam678 <gautam678@gmail.com>
2020-03-18 22:35:11 -0400

    make it responsive

--- components/Stats.js
+++ components/Stats.js
@@ -4,6 +4,10 @@ const StatGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 1rem;
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 0.5rem;
+  }
 `;
 
 const StatBlock = styled.div`
